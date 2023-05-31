10669bc19eb210117efca604dbf417be759e6344
João Marcelo Dantas <jmgrd98@gmail.com>
2023-05-31 11:59:58 -0300

    resetup vue

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -1,59 +1,30 @@
 <template>
-  
-  <div id="wrapper">
-    <nav class="navbar is-dark">
-      <div class="navbar-brand">
-        <router-link to="/" class="navbar-item">Djackets</router-link>
-
-        <a href="" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div class="navbar-menu" id="navbar-menu">
-        <div class="navbar-end">
-          <router-link to="/summer" class="navbar-item">Summer</router-link>
-          <router-link to="/winter" class="navbar-item">Winter</router-link>
-
-          <div class="navbar-item">
-            <div class="buttons">
-              <router-link to="/login" class="button is-light">
-                <strong>Login</strong>
-              </router-link>
-              <router-link to="/cart" class="button is-success">
-                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
-                <span>Cart</span>
-              </router-link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-
-    <section class="section">
-      <router-view/>
-    </section>
-
-    <footer class="footer">
-      <p class="has-text-centered">Copyright (c) 2023</p>
-    </footer>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
-</script>
-
-<style>
-@import '../node_modules/bulma/css/bulma.css';
-
 </style>
