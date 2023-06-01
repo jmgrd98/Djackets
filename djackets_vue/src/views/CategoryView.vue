92ee328c97759ed45b8f3a54480cf3c18bd478ba
João Marcelo Dantas <jmgrd98@gmail.com>
2023-06-01 17:51:18 -0300

    Update CategoryView.vue

--- djackets_vue/src/views/CategoryView.vue
+++ djackets_vue/src/views/CategoryView.vue
@@ -1,24 +1,10 @@
 <template>
     <div class="page-category">
-        <div class="container">
-            <div class="columns is-multiline">
-                <div class="column is-3"
-                v-for="product in category.products"
-                v-bind:key="product.id"
-                >
-                <div class="box">
-                  <figure class="image mb-4">
-                    <img :src="product.get_thumbnail">
-                  </figure>
-            
-                  <h3 class="is-size-4">{{ product.name }}</h3>
-                  <p class="is-size-6 has-text-centered">{{ product.price }}</p>
-                  <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">
-                    View details
-                  </router-link>
-            
-                </div>
-              </div>
+        <div class="column is-multiline">
+            <div class="column is-12">
+                <h2 class="is-size-2 has-text-centered">
+                    {{ category.name }}
+                </h2>
             </div>
         </div>
     </div>
@@ -45,7 +31,7 @@ export default {
             this.$store.commit('setIsLoading', true)
             const category_slug = this.$route.params.category_slug
 
-            await axios.get(`/api/v1/categories/${category_slug}/`)
+            await axios.get(`/api/v1/products/${category_slug}/`)
                 .then(response => {
                     this.category = response.data;
                     document.title = this.category.name + ' | Djackets'
